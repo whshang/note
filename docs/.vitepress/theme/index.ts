a439734815051387930f61cccf8c30ea806168ec
whshang <whshang@gmail.com>
2024-09-30 16:25:28 +0800

    测试增加AI搜索

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,10 +1,12 @@
+import { h } from 'vue'
+import { useData } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 
 // @ts-ignore 忽略下一行报错提示
 import MNavLinks from './components/MNavLinks.vue'
 
-import { h } from 'vue'
-import { useData } from 'vitepress'
+import Documate from '@documate/vue'
+import '@documate/vue/dist/style.css'
 
 export default {
   extends: DefaultTheme,
@@ -14,16 +16,9 @@ export default {
     app.component('MNavLinks' , MNavLinks)
   },
 
-  Layout: () => {
-    const props: Record<string, any> = {}
-    // 获取 frontmatter
-    const { frontmatter } = useData()
-
-    /* 添加自定义 class */
-    if (frontmatter.value?.layoutClass) {
-      props.class = frontmatter.value.layoutClass
-    }
-
-    return h(DefaultTheme.Layout, props)
-  },
+  Layout: h(DefaultTheme.Layout, null, {
+    'nav-bar-content-before': () => h(Documate, {
+      endpoint: '',
+    }),
+  }),
 }
