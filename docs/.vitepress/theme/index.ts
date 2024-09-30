75a351465ef1c6963a576d1eefe6f71493b73182
whshang <whshang@gmail.com>
2024-09-30 16:40:07 +0800

    移除AI

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -5,9 +5,6 @@ import DefaultTheme from 'vitepress/theme'
 // @ts-ignore 忽略下一行报错提示
 import MNavLinks from './components/MNavLinks.vue'
 
-import Documate from '@documate/vue'
-import '@documate/vue/dist/style.css'
-
 export default {
   extends: DefaultTheme,
 
@@ -16,9 +13,16 @@ export default {
     app.component('MNavLinks' , MNavLinks)
   },
 
-  Layout: h(DefaultTheme.Layout, null, {
-    'nav-bar-content-before': () => h(Documate, {
-      endpoint: '',
-    }),
-  }),
+  Layout: () => {
+    const props: Record<string, any> = {}
+    // 获取 frontmatter
+    const { frontmatter } = useData()
+
+    /* 添加自定义 class */
+    if (frontmatter.value?.layoutClass) {
+      props.class = frontmatter.value.layoutClass
+    }
+
+    return h(DefaultTheme.Layout, props)
+  },
 }
